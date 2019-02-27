4c8d5a697be09f6f709fb5dce6cb28c764c6aa21
Loey Agdan <loeyagdan0225@gmail.com>
2019-02-27 17:11:13 +0400

    Initial commit

---
+++ app/src/main/java/UtilityHelper/CognitoUserPool.kt
@@ -0,0 +1,14 @@
+package UtilityHelper
+
+class CognitoUserPool{
+
+    companion object {
+
+        val USERPOOL_ID = "us-east-2_rDd0DiSnB"
+        val CLIENT_ID = "5j1tnf52q7f422jhvtg53shd0"
+        val CLIENT_SECRET = "18c6rsdoru4kefnj095udf0ur74r6icjp5og5d6ugtqpik05n7nd"
+        val CLIENT_COGNITO_REGION = ""
+        val CLIENT_PINPOINT_APP_ID = ""
+
+    }
+}
