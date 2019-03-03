734b584ef9aa45c640a975f87cb8860b1935b084
Loey Agdan <loeyagdan0225@gmail.com>
2019-03-03 16:52:45 +0400

    + signin logic

--- app/src/main/java/invest/com/swapp/LoginActivity.kt
+++ app/src/main/java/invest/com/swapp/LoginActivity.kt
@@ -1,44 +1,34 @@
 package invest.com.swapp
 
-import UtilityHelper.CognitoUserPool
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.annotation.TargetApi
-import android.content.pm.PackageManager
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
-import android.app.LoaderManager.LoaderCallbacks
-import android.content.CursorLoader
-import android.content.Loader
-import android.database.Cursor
-import android.net.Uri
-import android.os.AsyncTask
-import android.os.Build
-import android.os.Bundle
-import android.provider.ContactsContract
-import android.view.View
-import android.view.inputmethod.EditorInfo
-import android.widget.ArrayAdapter
-import android.widget.TextView
 
-import java.util.ArrayList
-import android.Manifest.permission.READ_CONTACTS
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
 import android.content.Intent
 import android.util.Log
-import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUser
-import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserAttributes
-import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserCodeDeliveryDetails
-import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.SignUpHandler
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoDevice
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserPool
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserSession
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.AuthenticationContinuation
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.AuthenticationDetails
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.ChallengeContinuation
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.MultiFactorAuthenticationContinuation
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.AuthenticationHandler
 import com.amazonaws.regions.Regions
-
 import kotlinx.android.synthetic.main.activity_login.*
 import java.lang.Exception
 
-class LoginActivity : AppCompatActivity(), LoaderCallbacks<Cursor> {
+class LoginActivity : AppCompatActivity(){
 
- override fun onCreate(savedInstanceState: Bundle?) {
+    private var cUsername:String = ""
+    private var cPassword:String = ""
+    var userPool: CognitoUserPool? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
         password.setOnEditorActionListener(TextView.OnEditorActionListener { _, id, _ ->
             if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
                 attemptLogin()
@@ -47,53 +37,158 @@ class LoginActivity : AppCompatActivity(), LoaderCallbacks<Cursor> {
             false
         })
 
+        userPool = CognitoUserPool(baseContext,
+                UtilityHelper.CognitoUserPool.USERPOOL_ID,
+                UtilityHelper.CognitoUserPool.CLIENT_ID,
+                UtilityHelper.CognitoUserPool.CLIENT_SECRET,
+                Regions.US_EAST_2)
+
         email_sign_in_button.setOnClickListener { attemptLogin() }
-        email_sign_up_button.setOnClickListener { attemptSignup() }
+        email_sign_up_button.setOnClickListener { attemptSignup()}
 
-        val userPool = com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserPool(baseContext,CognitoUserPool.USERPOOL_ID,CognitoUserPool.CLIENT_ID,CognitoUserPool.CLIENT_SECRET,Regions.US_EAST_2)
-
-        var cognitoUserAttr = CognitoUserAttributes()
-        cognitoUserAttr.addAttribute("email","test@yahoo.com")
-        cognitoUserAttr.addAttribute("profile","FREE")
-        userPool.signUpInBackground("test@yahoo.com","test1234",cognitoUserAttr,null,handler)
     }
 
+    private fun attemptLogin() {
+        cUsername = txt_username.text.toString()
 
-    val handler = object: SignUpHandler{
-        override fun onSuccess(user: CognitoUser?, signUpConfirmationState: Boolean, cognitoUserCodeDeliveryDetails: CognitoUserCodeDeliveryDetails?) {
-           Log.d("_login","User successfully signed")
+//        if (cUsername.isEmpty()) {
+//            return
+//        }
+
+        cPassword = password.text.toString()
+
+//        if (cPassword.isEmpty()){
+//            return
+//        }
+
+        Log.d("signin","trigger")
+
+        userPool!!.getUser(cUsername).getSessionInBackground(handler)
+    }
+
+    var handler = object: AuthenticationHandler{
+        override fun onSuccess(userSession: CognitoUserSession?, newDevice: CognitoDevice?) {
+            val intent = Intent(baseContext, MasterActivity::class.java)
+            startActivity(intent)
         }
 
         override fun onFailure(exception: Exception?) {
-            Log.d("_login","Error user signed in" + exception.toString())
+
+        }
+
+        override fun authenticationChallenge(continuation: ChallengeContinuation?) {
+
+        }
+
+        override fun getAuthenticationDetails(authenticationContinuation: AuthenticationContinuation?, userId: String?) {
+            getUserAuthentication(authenticationContinuation!!,cUsername)
+        }
+
+        override fun getMFACode(continuation: MultiFactorAuthenticationContinuation?) {
+
         }
     }
 
+    fun getUserAuthentication(authenticationContinuation: AuthenticationContinuation,username:String){
+        var authDetails = AuthenticationDetails(username,cPassword,null)
+        authenticationContinuation.setAuthenticationDetails(authDetails)
+        authenticationContinuation.continueTask()
+    }
 
+    /*
 
+      if(username != null) {
+            this.username = username;
+            AppHelper.setUser(username);
+        }
+        if(this.password == null) {
+            inUsername.setText(username);
+            password = inPassword.getText().toString();
+            if(password == null) {
+                TextView label = (TextView) findViewById(R.id.textViewUserPasswordMessage);
+                label.setText(inPassword.getHint()+" enter password");
+                inPassword.setBackground(getDrawable(R.drawable.text_border_error));
+                return;
+            }
 
-    /**
-     * Callback received when a permissions request has been completed.
-     */
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
-                                            grantResults: IntArray) {
-        if (requestCode == REQUEST_READ_CONTACTS) {
-            if (grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
+            if(password.length() < 1) {
+                TextView label = (TextView) findViewById(R.id.textViewUserPasswordMessage);
+                label.setText(inPassword.getHint()+" enter password");
+                inPassword.setBackground(getDrawable(R.drawable.text_border_error));
+                return;
             }
         }
-    }
+        AuthenticationDetails authenticationDetails = new AuthenticationDetails(this.username, password, null);
+        continuation.setAuthenticationDetails(authenticationDetails);
+        continuation.continueTask();
 
 
-    private fun attemptLogin() {
+    */
 
-        Log.w("_message","Hello World")
-        Log.w("_message","username " + email.text + " password: " + password.text)
+    /*
 
-            val intent = Intent(baseContext, MasterActivity::class.java)
-            //startActivity(intent)
-            Log.w("_message","email is the same")
-    }
+     //
+    AuthenticationHandler authenticationHandler = new AuthenticationHandler() {
+        @Override
+        public void onSuccess(CognitoUserSession cognitoUserSession, CognitoDevice device) {
+            Log.d(TAG, " -- Auth Success");
+            AppHelper.setCurrSession(cognitoUserSession);
+            AppHelper.newDevice(device);
+            closeWaitDialog();
+            launchUser();
+        }
+
+        @Override
+        public void getAuthenticationDetails(AuthenticationContinuation authenticationContinuation, String username) {
+            closeWaitDialog();
+            Locale.setDefault(Locale.US);
+            getUserAuthentication(authenticationContinuation, username);
+        }
+
+        @Override
+        public void getMFACode(MultiFactorAuthenticationContinuation multiFactorAuthenticationContinuation) {
+            closeWaitDialog();
+            mfaAuth(multiFactorAuthenticationContinuation);
+        }
+
+        @Override
+        public void onFailure(Exception e) {
+            closeWaitDialog();
+            TextView label = (TextView) findViewById(R.id.textViewUserIdMessage);
+            label.setText("Sign-in failed");
+            inPassword.setBackground(getDrawable(R.drawable.text_border_error));
+
+            label = (TextView) findViewById(R.id.textViewUserIdMessage);
+            label.setText("Sign-in failed");
+            inUsername.setBackground(getDrawable(R.drawable.text_border_error));
+
+            showDialogMessage("Sign-in failed", AppHelper.formatException(e));
+        }
+
+        @Override
+        public void authenticationChallenge(ChallengeContinuation continuation) {
+            /**
+             * For Custom authentication challenge, implement your logic to present challenge to the
+             * user and pass the user's responses to the continuation.
+             */
+            if ("NEW_PASSWORD_REQUIRED".equals(continuation.getChallengeName())) {
+                // This is the first sign-in attempt for an admin created user
+                newPasswordContinuation = (NewPasswordContinuation) continuation;
+                AppHelper.setUserAttributeForDisplayFirstLogIn(newPasswordContinuation.getCurrentUserAttributes(),
+                        newPasswordContinuation.getRequiredAttributes());
+                closeWaitDialog();
+                firstTimeSignIn();
+            } else if ("SELECT_MFA_TYPE".equals(continuation.getChallengeName())) {
+                closeWaitDialog();
+                mfaOptionsContinuation = (ChooseMfaContinuation) continuation;
+                List<String> mfaOptions = mfaOptionsContinuation.getMfaOptions();
+                selectMfaToSignIn(mfaOptions, continuation.getParameters());
+            }
+        }
+    };
+
+
+    */
 
     private fun attemptSignup(){
         val intent = Intent(baseContext, SignupActivity::class.java)
@@ -110,59 +205,6 @@ class LoginActivity : AppCompatActivity(), LoaderCallbacks<Cursor> {
         return password.length > 4
     }
 
-
-
-    override fun onCreateLoader(i: Int, bundle: Bundle?): Loader<Cursor> {
-        return CursorLoader(this,
-                // Retrieve data rows for the device user's 'profile' contact.
-                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
-                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
-
-                // Select only email addresses.
-                ContactsContract.Contacts.Data.MIMETYPE + " = ?", arrayOf(ContactsContract.CommonDataKinds.Email
-                .CONTENT_ITEM_TYPE),
-
-                // Show primary email addresses first. Note that there won't be
-                // a primary email address if the user hasn't specified one.
-                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC")
-    }
-
-    override fun onLoadFinished(cursorLoader: Loader<Cursor>, cursor: Cursor) {
-        val emails = ArrayList<String>()
-        cursor.moveToFirst()
-        while (!cursor.isAfterLast) {
-            emails.add(cursor.getString(ProfileQuery.ADDRESS))
-            cursor.moveToNext()
-        }
-
-        addEmailsToAutoComplete(emails)
-    }
-
-    override fun onLoaderReset(cursorLoader: Loader<Cursor>) {
-
-    }
-
-    private fun addEmailsToAutoComplete(emailAddressCollection: List<String>) {
-        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-        val adapter = ArrayAdapter(this@LoginActivity,
-                android.R.layout.simple_dropdown_item_1line, emailAddressCollection)
-
-        email.setAdapter(adapter)
-    }
-
-    object ProfileQuery {
-        val PROJECTION = arrayOf(
-                ContactsContract.CommonDataKinds.Email.ADDRESS,
-                ContactsContract.CommonDataKinds.Email.IS_PRIMARY)
-        val ADDRESS = 0
-        val IS_PRIMARY = 1
-    }
-
-
-    companion object {
-        private val REQUEST_READ_CONTACTS = 0
-        private val DUMMY_CREDENTIALS = arrayOf("foo@example.com:hello", "bar@example.com:world")
-    }
 
 
 
