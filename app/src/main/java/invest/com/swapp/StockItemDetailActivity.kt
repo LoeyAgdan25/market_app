04859b92fa10412dd4f02ebbb00ed94e02f8c984
Loey Agdan <loeyagdan0225@gmail.com>
2019-03-20 11:13:15 +0400

    + db helper

--- app/src/main/java/invest/com/swapp/StockItemDetailActivity.kt
+++ app/src/main/java/invest/com/swapp/StockItemDetailActivity.kt
@@ -1,15 +1,25 @@
 package invest.com.swapp
 
+import android.content.ContentValues
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.MenuItem
+import invest.com.swapp.db.database
 import kotlinx.android.synthetic.main.activity_stockitem_detail.*
 import kotlinx.android.synthetic.main.content_detail_stocks.*
+import org.jetbrains.anko.custom.async
+import org.jetbrains.anko.db.insert
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.toast
+import java.util.*
 
 
 class StockItemDetailActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,6 +60,27 @@ class StockItemDetailActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own detail action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        btn_watch_stock.setOnClickListener { doWatchStock() }
+    }
+
+    fun doWatchStock(){
+
+        Log.d("event","watching stock");
+
+//        doAsync {
+            database.use {
+                insert("tblWatched",
+                        "symbol" to txt_stock_symbol.text,
+                        "name" to intent.getStringExtra(StockItemDetailFragment.ARG_ITEM_NAME),
+                        "currency" to "PHP",
+                        "amount" to txt_stock_price.text,
+                        "volume" to txt_stock_volume.text
+                )
+
+                toast("stock is saved!")
+            }
+//        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem) =
---
+++ app/src/main/java/invest/com/swapp/db/DBHelper.kt
@@ -0,0 +1,40 @@
+package invest.com.swapp.db
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import org.jetbrains.anko.db.*
+
+class DBHelper(contxt:Context): ManagedSQLiteOpenHelper(contxt,"SwappDB", null,1){
+
+    //Singleton
+    companion object {
+        private var instance: DBHelper? = null
+
+        @Synchronized
+        fun getInstance(ctx: Context): DBHelper{
+            if(instance == null){
+                instance = DBHelper(ctx.applicationContext)
+            }
+
+            return instance!!
+        }
+    }
+
+    override fun onCreate(p0: SQLiteDatabase?) {
+        p0!!.createTable("tblWatched",true,
+                "id" to INTEGER + PRIMARY_KEY + UNIQUE,
+                "symbol" to TEXT,
+                "name" to TEXT,
+                "currency" to TEXT,
+                "amount" to TEXT,
+                "volume" to TEXT
+                )
+    }
+
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        p0!!.dropTable("tblWatched",true)
+    }
+}
+
+val Context.database: DBHelper get() = DBHelper.getInstance(applicationContext)
