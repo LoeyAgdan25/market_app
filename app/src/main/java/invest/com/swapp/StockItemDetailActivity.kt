1efbd455b65c5e473c20ea810d0d9afa453e3a0b
Loey Agdan <loeyagdan0225@gmail.com>
2019-03-21 11:34:48 +0400

    + unwatched button & function

--- app/src/main/java/invest/com/swapp/StockItemDetailActivity.kt
+++ app/src/main/java/invest/com/swapp/StockItemDetailActivity.kt
@@ -12,6 +12,7 @@ import android.view.View
 import invest.com.swapp.db.database
 import kotlinx.android.synthetic.main.activity_stockitem_detail.*
 import kotlinx.android.synthetic.main.content_detail_stocks.*
+import org.jetbrains.anko.alert
 import org.jetbrains.anko.custom.async
 import org.jetbrains.anko.db.*
 import org.jetbrains.anko.doAsync
@@ -64,6 +65,7 @@ class StockItemDetailActivity : AppCompatActivity() {
         }
 
         btn_watch_stock.setOnClickListener { doWatchStock() }
+        btn_watch_remove.setOnClickListener { doRemoveWatched(symbol) }
         doFindStock(symbol)
         doFindStockAll()
     }
@@ -79,6 +81,28 @@ class StockItemDetailActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    fun doRemoveWatched(sym:String){
+
+        alert {
+            title("Remove Stock")
+            message("Remove Stock from watchlist")
+            yesButton {
+                database.use {
+                    delete("tblWatched","symbol = {symbol}", "symbol" to sym)
+                }
+
+                btn_watch_stock.visibility = View.VISIBLE
+                btn_watch_remove.visibility = View.GONE
+            }
+
+            noButton{
+                //do nothing
+            }
+        }.show()
+
+
     }
 
     fun doFindStock(sym: String){
@@ -102,9 +126,7 @@ class StockItemDetailActivity : AppCompatActivity() {
 
     fun doWatchStock(){
 
-        Log.d("event","watching stock");
-
-//        doAsync {
+        Log.d("event","watching stock")
             database.use {
                insert("tblWatched",
                         "symbol" to txt_stock_symbol.text,
@@ -116,8 +138,8 @@ class StockItemDetailActivity : AppCompatActivity() {
                 )
 
                 toast("stock is saved!")
+                btn_watch_stock.visibility = View.GONE
             }
-//        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem) =
