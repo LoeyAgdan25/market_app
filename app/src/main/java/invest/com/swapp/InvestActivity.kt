6c8e00090390899df1bb3e58daa1138e5ba10aae
Loey Agdan <loeyagdan0225@gmail.com>
2019-03-26 14:00:17 +0400

    + computation -m ui to invest activity

--- app/src/main/java/invest/com/swapp/InvestActivity.kt
+++ app/src/main/java/invest/com/swapp/InvestActivity.kt
@@ -2,6 +2,12 @@ package invest.com.swapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_invest.*
+import kotlinx.android.synthetic.main.content_detail_stocks.*
+import org.jetbrains.anko.toast
 
 class InvestActivity : AppCompatActivity() {
 
@@ -11,5 +17,76 @@ class InvestActivity : AppCompatActivity() {
 
         supportActionBar!!.title = intent.getStringExtra("symbol")
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+
+        txt_price_sold.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int,count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int,before: Int, count: Int) {doCompute()}
+        })
+
+        txt_stocks_sold.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int,count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int,before: Int, count: Int) {doCompute()}
+        })
+
+        txt_broker_charge.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int,count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int,before: Int, count: Int) {doCompute()}
+        })
+
+        txt_tax_charge.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int,count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int,before: Int, count: Int) {doCompute()}
+        })
+
     }
+
+    fun doCompute(){
+
+        Log.d("_log","doCompute()")
+
+        var price = 0.00
+        var stock = 0.00
+        var amount = 0.00
+        var charge = 0.00
+        var tax = 0.00
+
+        if(txt_price_sold.text.isEmpty()){
+            price = 0.00
+        }else{
+            price = txt_price_sold.text.toString().toDouble()
+        }
+
+        if(txt_stocks_sold.text.isEmpty()){
+            stock = 0.00
+        }else{
+            stock = txt_stocks_sold.text.toString().toDouble()
+        }
+
+        if(txt_broker_charge.text.isEmpty()){
+            stock = 0.00
+        }else{
+            stock = txt_broker_charge.text.toString().toDouble()
+        }
+
+        if(txt_tax_charge.text.isEmpty()){
+            tax = 0.00
+        }else{
+            tax = txt_tax_charge.text.toString().toDouble()
+        }
+
+        Log.d("_logs", "$price $stock")
+        amount = (price * stock)
+        txt_amount_sold.setText("$amount")
+
+
+        var allTotal = (price * stock) - (charge + tax)
+        txt_total_investment.setText("$allTotal")
+
+    }
+
 }
