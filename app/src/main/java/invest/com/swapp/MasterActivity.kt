451549f0d7992006da77ed476a82717d742df19e
Loey Agdan <loeyagdan0225@gmail.com>
2019-03-31 14:36:11 +0400

    + add loading watchlist

--- app/src/main/java/invest/com/swapp/MasterActivity.kt
+++ app/src/main/java/invest/com/swapp/MasterActivity.kt
@@ -1,10 +1,12 @@
 package invest.com.swapp
 
 import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import invest.com.swapp.adapter.RecyclerAdapter
@@ -12,7 +14,14 @@ import invest.com.swapp.auth.LoginActivity
 import invest.com.swapp.db.DBHelper
 import invest.com.swapp.db.database
 import kotlinx.android.synthetic.main.activity_master.*
+import kotlinx.coroutines.experimental.DefaultDispatcher
+import kotlinx.coroutines.*
+import okhttp3.*
 import org.jetbrains.anko.db.select
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.toast
+
+import java.io.IOException
 import kotlin.system.exitProcess
 
 class MasterActivity : AppCompatActivity(){
@@ -21,6 +30,8 @@ class MasterActivity : AppCompatActivity(){
     private lateinit var linearLayoutManager: LinearLayoutManager
     private lateinit var adapter: RecyclerAdapter
     private val stockListAll = ArrayList<Stock>()
+    private lateinit var client: OkHttpClient
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,26 +43,64 @@ class MasterActivity : AppCompatActivity(){
         adapter = RecyclerAdapter(stockList)
         recyclerViewMain.adapter = adapter
 
+
+        client = OkHttpClient()
         btn_dashboard_search.setOnClickListener { doSearchStock() }
         setUpRecyclerview(recyclerViewMain)
+
+
+
+
     }
+
+
 
     private fun setUpRecyclerview(recyclerView: RecyclerView){
         //do sqlite database
-
+        //add grouping wait
+        //TODO:- Do coroutines...
         database.use {
             select(DBHelper.tblWatchlist).exec {
                 while (moveToNext()){
                     val stockModel = Stock("","${getString(getColumnIndex("symbol"))}","","","","")
+
+                    doAsync { fetchUpdate(stockModel.symbol) }
                     stockListAll.add(stockModel)
                 }
             }
         }
 
+        //Add group wait here...
+
         if(stockListAll.size > 0){
             recyclerView.adapter = RecyclerAdapter(stockListAll)
         }
+    }
 
+    private fun fetchUpdate(symbol: String){
+
+       val urlRequest = Uri.Builder().scheme(MasterActivity.URL_SCHEME)
+               .authority(MasterActivity.URL_AUTHORITY)
+               .appendPath("stocks")
+               .appendPath("$symbol.json")
+               .build().toString()
+
+
+        Log.d("_logs","symbol $symbol $urlRequest")
+        val request = Request.Builder().url(urlRequest).build()
+
+        client.newCall(request).enqueue(object : Callback{
+            override fun onResponse(call: Call, response: Response) {
+                Log.d("_log", response.body()!!.string())
+//                toast("response $response")
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+//                toast("error $e")
+                Log.d("_log", "$e")
+            }
+
+        })
     }
 
     fun doSearchStock(){
@@ -76,5 +125,12 @@ class MasterActivity : AppCompatActivity(){
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+    //http://phisix-api2.appspot.com/stocks/BDO.json
+    companion object {
+        private val URL_SCHEME = "http"
+        private val URL_AUTHORITY = "phisix-api2.appspot.com"
+        private val URL_PATH_1 = "stocks.json"
+
     }
 }
