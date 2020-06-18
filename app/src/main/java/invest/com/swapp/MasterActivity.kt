aa5294e747726225d87f3ef1268cf3069a2d19c9
Loey <loeyagdan25@gmail.com>
2020-06-19 00:15:45 +0400

    * fix watchlist loading * edit account update broker link

--- app/src/main/java/invest/com/swapp/MasterActivity.kt
+++ app/src/main/java/invest/com/swapp/MasterActivity.kt
@@ -1,6 +1,8 @@
 package invest.com.swapp
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -16,8 +18,8 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
-import com.google.android.material.button.MaterialButton
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputEditText
 import invest.com.swapp.adapter.WatchedRecyclerAdapter
 import invest.com.swapp.auth.LoginActivity
 import invest.com.swapp.listener.WatchListener
@@ -26,10 +28,10 @@ import invest.com.swapp.viewmodel.AuthViewModel
 import invest.com.swapp.viewmodel.StocksViewModel
 import kotlinx.android.synthetic.main.activity_master.*
 import kotlinx.android.synthetic.main.layout_buy_sell_prompt.view.*
-import kotlinx.android.synthetic.main.layout_user_profile_account.*
 import kotlinx.android.synthetic.main.layout_watchlist_notification.view.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import org.jetbrains.anko.toast
 
 //todo:- transfer to new master activity
 // fragmented view pager2
@@ -39,7 +41,7 @@ class MasterActivity : AppCompatActivity(), WatchListener{
     //todo:- workmanager to update watchlist
 
     private lateinit var linearLayoutManager: LinearLayoutManager
-    private lateinit var adapter: WatchedRecyclerAdapter
+    private lateinit var watchListAdapter: WatchedRecyclerAdapter
     lateinit var mAdView : AdView
     var watchListener:WatchListener? = null
     val listen: MutableLiveData<List<String>> = MutableLiveData()
@@ -47,23 +49,28 @@ class MasterActivity : AppCompatActivity(), WatchListener{
     /*mvvm*/
     private lateinit var stockViewModel: StocksViewModel
     private lateinit var authViewModel: AuthViewModel
+    private lateinit var watchList:ArrayList<StocksWatched>
+    private lateinit var pref:SharedPreferences
 
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_master)
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713")
+
         title = ""
         supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_action_account)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         watchListener = this
 
+        pref = getSharedPreferences("sharedPreferences",Context.MODE_PRIVATE)
 
         //Production ca-app-pub-4268048783942748~4717310066
         //Testing ca-app-pub-3940256099942544~3347511713
         //Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
 
-        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713")
+
         linearLayoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         recyclerViewMain.layoutManager = linearLayoutManager
 
@@ -73,14 +80,18 @@ class MasterActivity : AppCompatActivity(), WatchListener{
         val adRequest = AdRequest.Builder().build()
         mAdView.loadAd(adRequest)
 
+        watchList = ArrayList()
+        watchListAdapter = WatchedRecyclerAdapter(watchList)
+        watchListAdapter.watchListener = this
+        recyclerViewMain.adapter = watchListAdapter
 
         /*mvvm*/
         authViewModel = ViewModelProviders.of(this).get(AuthViewModel::class.java)
         stockViewModel = ViewModelProviders.of(this).get(StocksViewModel::class.java)
         stockViewModel.watchedStocks.observe(this, Observer {
-                adapter = WatchedRecyclerAdapter(it)
-                adapter.watchListener = this
-                recyclerViewMain.adapter = adapter
+                watchList.clear()
+                watchList.addAll(it)
+                recyclerViewMain!!.adapter!!.notifyDataSetChanged()
                             for(watch: StocksWatched in it){
                                 if(watch.buy_price == watch.price.toFloat()){
                                     //toast("buy price marked is reached ${watch.symbol}")
@@ -183,11 +194,24 @@ class MasterActivity : AppCompatActivity(), WatchListener{
                 }
                 android.R.id.home ->{
                     var view = layoutInflater.inflate(R.layout.layout_user_profile_account, null)
+                    var txtLink = view.findViewById<TextInputEditText>(R.id.txt_broker_url)
+
+                    txtLink.setText(pref.getString("_broker",""))
+
                     MaterialAlertDialogBuilder(MasterActivity@this, R.style.AlertDialogTheme)
                             .setView(view)
                             .setPositiveButton("Save"){
                                 dialog, which ->
-                                //save to shared preference
+                                with(pref.edit()){
+                                    if(!txtLink.text.toString().contains("https://")){
+                                        putString("_broker", "https://" + txtLink.text.toString())
+                                    }else {
+                                        putString("_broker", txtLink.text.toString())
+                                    }
+                                    commit()
+                                }
+                                toast("broker save")
+
                             }
                             .setNegativeButton("Open"){
                                 dialog, which ->
@@ -237,11 +261,6 @@ class MasterActivity : AppCompatActivity(), WatchListener{
                             stockViewModel.deleteWatched(stockWatched)
                         }
                     }
-//                    .setNeutralButton("Cancel"){
-//                        dialog, _ ->
-//                                    dialog.cancel()
-//                            }
-//                            .setCancelable(false)
                     .show()
     }
 
@@ -255,9 +274,16 @@ class MasterActivity : AppCompatActivity(), WatchListener{
     }
 
     fun openBroker(){
-        val intent = Intent(this, ViewNews::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-        intent.putExtra("link", "https://www.colfinancial.com") //get this from shared preference
-        startActivity(intent)
+        val brokerLink = pref.getString("_broker","")
+
+        if(brokerLink.isNullOrBlank() || brokerLink == "https://"){
+                //check if valid url
+                toast("broker settings not set")
+        }else{
+            val intent = Intent(this, ViewNews::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            intent.putExtra("link", brokerLink) //get this from shared preference
+            startActivity(intent)
+        }
     }
 }
